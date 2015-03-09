bd9e7fd0e24e19101c9d1ac492a6f543d48a397d
jarl-alejandro <jarlalejor@gmail.com>
2015-03-09 11:13:28 -0500

    delete file functions of app users

--- users/functions.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth import authenticate, login
-from django.shortcuts import redirect
-
-def LogIn(request, email, password):
-	user = authenticate(email = email, password = password)
-	if user is None:
-		if user.is_active:
-			login(request, user) 
