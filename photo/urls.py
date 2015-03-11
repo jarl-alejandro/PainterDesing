4188c8547fce8d8709c50641022c79ffefd90c4e
jarl-alejandro <jarlalejor@gmail.com>
2015-03-10 21:00:36 -0500

    add views upload photo

--- photo/urls.py
+++ photo/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-from .views import LandingView, App
+from .views import LandingView, App, UploadPhotView
 
 urlpatterns = patterns('',
 	url(r'^$', LandingView.as_view(), name="landing"),
 	url(r'^app/$', App.as_view(), name="app"),
+	url(r'^upload/$', UploadPhotView.as_view(), name="upload"),
 )
--- photo/views.py
+++ photo/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 
 from .mixins import LoginRequired
+from .models import Photo
+from .forms import UploadPhotoForm
 
 class LandingView(TemplateView):
 	template_name = "landing.html"
 
 class App(LoginRequired, TemplateView):
 	template_name = "app.html"
+
+	def get_context_data(self, **kwargs):
+		context = super(App, self).get_context_data(**kwargs)
+		photos = Photo.objects.all()
+
+		data = { 'photos':photos }
+
+		context.update(data)
+		return context
+
+class UploadPhotView(LoginRequired, CreateView):
+	model = Photo
+	form_class = UploadPhotoForm
+	template_name = "upload.html"
+	success_url = "/app/"
+
+	def form_valid(self, form):
+		form.instance.user = self.request.user
+		return super(UploadPhotView, self).form_valid(form)
